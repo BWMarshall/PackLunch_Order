135ddac4996267f24f113402ec72c19e73edd406
BWMarshall19 <marshall.bw@oundleschool.org.uk>
2020-11-14 09:15:07 +0000

    P1 (14th)

---
+++ Connection.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+
+     
+
+    <title>Page title</title> 
+
+     
+
+</head> 
+<body> 
+
+    <?php 
+        $servername = "localhost"; 
+        $username = "root"; 
+        $password = ""; 
+        $dbname = "packlunch"; 
+        try { 
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password); 
+            // set the PDO error mode to exception 
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+            echo "Connected successfully <br>";  
+            } 
+        catch(PDOException $e) 
+            { 
+            echo "Connection failed: " . $e->getMessage(); 
+            } 
+    ?>   
+
+</body> 
+</html> 
---
+++ Install.php
@@ -0,0 +1,111 @@
+<?php
+
+$servername = 'localhost';
+
+$username = 'root';
+
+$password= '';
+
+//note no Database mentioned here!
+
+try {
+
+ $conn = new PDO("mysql:host=$servername", $username, $password);
+
+ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+ $sql = "CREATE DATABASE IF NOT EXISTS packlunch";
+
+ $conn->exec($sql);
+
+ //next 3 lines optional only needed really if you want to go on an do more SQL here!
+
+ $sql = "USE packlunch";
+
+ $conn->exec($sql);
+
+ echo "DB created successfully";
+
+}
+
+catch(PDOException $e)
+
+{
+
+ echo $sql . "<br>" . $e->getMessage();
+
+}
+
+
+
+include_once("connection.php");
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS Tbl_MealOptions;
+
+CREATE TABLE Tbl_MealOptions 
+
+(FoodID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+
+Type TINYINT(1) NOT NULL,
+
+Name(20) NOT NULL,
+
+Avalability INT(4) NOT NULL,
+
+ReorderLevel INT(4) NOT NULL)");
+
+$stmt->execute();
+
+$stmt->closeCursor();
+
+
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS Tbl_Users;
+
+CREATE TABLE Tbl_Users 
+
+(Username VARCHAR(20)  PRIMARY KEY,
+
+Password VARCHAR(20) NOT NULL,
+
+Forename VARCHAR(20) NOT NULL,
+
+Surname VARCHAR(20) NOT NULL,
+
+House VARCHAR(20) NOT NULL,
+
+Year INT(4),
+
+Role TINYINT(1))");
+
+$stmt->execute();
+
+$stmt->closeCursor();
+
+
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS Tbl_Orders;
+
+CREATE TABLE Tbl_Orders
+
+(Username VARCHAR(20),
+
+SWID INT(4),
+
+SNID INT(4),
+
+DKID CHAR(4),
+
+Date DATE(2),
+
+MealMissed TINYINT(1),
+
+PRIMARY KEY(Username,Date,MealMissed))");
+
+$stmt->execute();
+
+$stmt->closeCursor();
+
+?>
+
+
