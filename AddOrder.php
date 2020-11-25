09c95636e5acac42c45f63a1a50cb6472fcf402a
BWMarshall19 <marshall.bw@oundleschool.org.uk>
2020-11-25 09:32:39 +0000

    Change av

--- AddOrder.php
+++ AddOrder.php
@@ -43,7 +43,59 @@ if (!isset($_SESSION['Name']))
     $stmt->bindParam(':date', $_POST["date"]);
     $stmt->bindParam(':mealm', $_POST["mm"]);
     $stmt->execute();
+  
+
+    ///Reducing the availability by 1 for each item ordered
+    //SW -------------------------------------------------------------------------------------
+    $stmt = $conn->prepare("SELECT Availability from tbl_mealoptions WHERE FoodID = :swid");
+    $stmt->bindParam(':swid', $_POST["SWid"]);   
+    $stmt->execute();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $SWNA = $row["Availability"] - 1;
+    }
+    echo($SWNA);
+
+    array_map("htmlspecialchars", $_POST);
+    $stmt = $conn->prepare("UPDATE Tbl_mealoptions SET Availability = :swna WHERE FoodID = :swid");
+    $stmt->bindParam(':swid', $_POST["SWid"]);
+    $stmt->bindParam(':swna', $SWNA);    
+    $stmt->execute();
+
+
+    //SN ----------------------------------------------------------------------------
+    $stmt = $conn->prepare("SELECT Availability from tbl_mealoptions WHERE FoodID = :snid");
+    $stmt->bindParam(':snid', $_POST["SNid"]);   
+    $stmt->execute();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $SWNA = $row["Availability"] - 1;
+    }
+    echo($SWNA);
+
+    array_map("htmlspecialchars", $_POST);
+    $stmt = $conn->prepare("UPDATE Tbl_mealoptions SET Availability = :snna WHERE FoodID = :snid");
+    $stmt->bindParam(':snid', $_POST["SNid"]);
+    $stmt->bindParam(':snna', $SWNA);    
+    $stmt->execute();
+
+    //DK ----------------------------------------------------------------------------
+    $stmt = $conn->prepare("SELECT Availability from tbl_mealoptions WHERE FoodID = :dkid");
+    $stmt->bindParam(':dkid', $_POST["DKid"]);   
+    $stmt->execute();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $SWNA = $row["Availability"] - 1;
+    }
+    echo($SWNA);
+
+    array_map("htmlspecialchars", $_POST);
+    $stmt = $conn->prepare("UPDATE Tbl_mealoptions SET Availability = :dkna WHERE FoodID = :dkid");
+    $stmt->bindParam(':dkid', $_POST["DKid"]);
+    $stmt->bindParam(':dkna', $SWNA);    
+    $stmt->execute();
     $conn=null;
+
     header('Location: CreateOrder.php');
 ?> 
 
